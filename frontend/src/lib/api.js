bf7fa2abb840b6d02ed999beff6e3ddad868de90
Alouzious <alouzious@gmail.com>
2026-07-30 10:14:30 +0300

    my render services was suspended now added new services

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://kabai.onrender.com/api/v1",
+  baseURL: "https://kabai-c2ox.onrender.com/api/v1",
+  // baseURL: "https://kabai.onrender.com/api/v1",
   // baseURL: "http://localhost:8000/api/v1",
 });
 
